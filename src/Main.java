a2a3c025e7b5bca347eb31b3ec90d2e2ffd1e878
Jason D'Oyley <jdfdoyley@gmail.com>
2022-12-13 05:19:24 -0500

    Write a program to divide to numbers and print the results

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 /*
 * Program: Write Java program to divide two numbers and print the screen.
 * Date: December 12, 2022
 * */
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input number one: ");
+        int num1 = input.nextInt();
+        System.out.print("Input number two: ");
+        int num2 = input.nextInt();
 
+        System.out.println("\nQuotient: " + (num1/num2));
     }
 }
