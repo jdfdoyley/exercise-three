ce6e87cb5259bd7702a5f81a22a069177ce41920
Jason D'Oyley <jdfdoyley@gmail.com>
2022-12-12 20:53:51 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/*
+* Program: Write Java program to divide two numbers and print the screen.
+* Date: December 12, 2022
+* */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
